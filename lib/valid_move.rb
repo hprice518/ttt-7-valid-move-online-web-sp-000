581cc6a7486f54e9c4a18390713423f32d3988f3
Harrison Price <hprice518@gmail.com>
2020-11-01 00:32:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,8 +12,6 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-#  if position_taken?
-#   return false
   if index.between?(0,8) || position_taken?(board, index)
     return false
   end
